b155645cdeb42ad83c2f9aac06530dddefaafb16
Diksha Rani <83601273+Dik17@users.noreply.github.com>
2023-10-09 09:49:38 +0530

    Update Profile.js

--- src/Patient/profile/Profile.js
+++ src/Patient/profile/Profile.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Profile.css';
 import { Box,Button,Typography } from '@material-ui/core';
 import PatientSidebar from '../PatientSidebar';
+import { RiEdit2Line } from "react-icons/ri";
 const Profile = () => {
   return (
     <>
@@ -46,42 +47,38 @@ const Profile = () => {
               <Box sx={{width:'30%',height:'50%',marginLeft:'200px',marginTop:'200px', border: '1px solid transparent',textAlign:'center',alignItems:'left'}}>
               <Typography sx={{ fontSize: "22px", height: "40px" }}>
                    NAME: 
+                   <input style={{marginLeft:'600px'}}></input>
                   </Typography>
                   <br/>
                   <Typography sx={{ fontSize: "22px", height: "40px" }}>
                GENDER: 
+               <input style={{marginLeft:'600px'}}></input>
                   </Typography>
                   <br/>
                   <Typography sx={{ fontSize: "22px", height: "40px" }}>
                BLOOD GROUP: 
+               <input style={{marginLeft:'600px'}}></input>
                   </Typography>
                   <br/>
                   <Typography sx={{ fontSize: "22px", height: "40px" }}>
                ADDRESS:
+               <input style={{marginLeft:'600px'}}></input>
                   </Typography>
                   <br/>
                   <Typography sx={{ fontSize: "22px", height: "40px" }}>
                CONTACT NUMBER:
+               <input style={{marginLeft:'600px'}}></input>
                   </Typography>
-                  <br/>
-                  <Typography sx={{ fontSize: "22px", height: "40px" }}>
-               BLOOD GROUP: 
-                  </Typography>
-                  <br/>
-                  <Typography sx={{ fontSize: "22px", height: "40px" }}>
-               BLOOD GROUP: 
-                  </Typography>
-                  <br/>
                  
                   <Box >
-                 <Button variant='contained' style={{marginRight:'20px'}}>
+                 <Button variant='contained'  style={{marginRight:'20px',backgroundColor:'#82A2C5',marginTop:'50px',width:'20%'}}>
                   SAVE
                  </Button>
                  </Box>
 
                  <Box>
-                 <Button variant='contained' style={{marginLeft:'600px',marginTop:'-30px'}}>
-                  EDIT
+                 <Button variant='contained' style={{marginLeft:'600px',marginTop:'-40px',width:'20%',backgroundColor:'#82A2C5'}}>
+                  <RiEdit2Line></RiEdit2Line>EDIT
                  </Button>
                  </Box>
                  </Box>
